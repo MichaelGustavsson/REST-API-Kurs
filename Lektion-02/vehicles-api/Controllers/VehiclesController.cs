13d9a979ae9d24a9ee61b999ddc2a1691aa4f84d
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-11-28 14:16:53 +0100

    Update solution with DELETE and PUT

--- Lektion-02/vehicles-api/Controllers/VehiclesController.cs
+++ Lektion-02/vehicles-api/Controllers/VehiclesController.cs
@@ -45,6 +45,38 @@ public class VehiclesController : ControllerBase
   public ActionResult AddVehicle(Vehicle vehicle)
   {
     // 1. Vi måste få in det data som ska sparas✅
+    var newVehicle = Create(vehicle);
+
+    // 4. Returnera korrekt statuskod till avsändaren (201 Created)...✅
+    // HATEOS...
+    // Mer korrekt sätta att returnera statuskod 201...
+    // CreatedAtAction gör följande åt oss
+    // 1. Skapar en statuskod 201,
+    // 2. Första argumentet skapa en url för metoden FindVehicle
+    //    http://localhost:5001/api/v1/vehicles/
+    //    Andra argumentet skapar argumentet som FindVehicle behöver (int id)
+    //    http://localhost:5001/api/v1/vehicles/20
+    //    tredje argumentet kommer att returnera vår nya bil i body delen av http resultet...
+    return CreatedAtAction(nameof(FindVehicle), new { id = newVehicle.Id }, newVehicle);
+    // return Created("http://localhost:5001/api/v1/vehicles/" + vehicle.Id, vehicle);
+  }
+
+  [HttpDelete("{id}")]
+  public ActionResult DeleteVehicle(int id)
+  {
+    Remove(id);
+    return NoContent();
+  }
+
+  [HttpPut("{id}")]
+  public ActionResult UpdateVehicle(Vehicle vehicle, int id)
+  {
+    Update(id, vehicle);
+    return NoContent();
+  }
+
+  private Vehicle Create(Vehicle vehicle)
+  {
     // 2. Skapa kod för att lägga till en ny bil i vår json fil...✅
     var path = string.Concat(_environment.ContentRootPath, "/Data/vehicles.json");
     var vehicles = Storage<Vehicle>.ReadJson(path);
@@ -57,11 +89,42 @@ public class VehiclesController : ControllerBase
     // 3. Skriv ner den uppdaterade listan till json filen...✅
     Storage<Vehicle>.WriteJson(path, vehicles);
 
-    // 4. Returnera korrekt statuskod till avsändaren (201 Created)...✅
-    // HATEOS...
-    // Mer korrekt sätta att returnera statuskod 201...
-    return CreatedAtAction(nameof(FindVehicle), new { id = vehicle.Id }, vehicle);
-    // return Created("http://localhost:5001/api/v1/vehicles/" + vehicle.Id, vehicle);
+    return vehicle;
   }
 
+  private void Remove(int id)
+  {
+    var path = string.Concat(_environment.ContentRootPath, "/Data/vehicles.json");
+    var vehicles = Storage<Vehicle>.ReadJson(path);
+
+    // 1. Leta upp bilen som ska raderas...
+    // Lambda (vehicle => vehicle.Id == id)
+    // första delen i lambda är en variabel(ni hittar på den själv)
+    // det som står till höger om pilen(=>) är funktionen som ska köras...
+    var toDelete = vehicles.SingleOrDefault(vehicle => vehicle.Id == id);
+    // 2. Ta bort bilen ur listan...
+    // Vi använder en inbyggda metoden Remove som finns i typen List i .NET.
+    vehicles.Remove(toDelete);
+
+    // 3. Uppdatera json dokumentet med den nya listan...
+    Storage<Vehicle>.WriteJson(path, vehicles);
+  }
+
+  private void Update(int id, Vehicle updatedVehicle)
+  {
+    // 1. Hämta alla bilar igen!...
+    var path = string.Concat(_environment.ContentRootPath, "/Data/vehicles.json");
+    var vehicles = Storage<Vehicle>.ReadJson(path);
+
+    // 2. Filtrera listan av bilar och bara returnera de bilar som inte har
+    //    id för den bil vi ska uppdatera...
+    // Skapar en ny lista minus den bil som ska uppdateras.
+    var filteredList = vehicles.FindAll(vehicle => vehicle.Id != id);
+
+    // 3. Lägg till bilen som är uppdaterad till den filtrerade listan...
+    filteredList.Add(updatedVehicle);
+
+    // 4. Skriv ner den nya listan...
+    Storage<Vehicle>.WriteJson(path, filteredList);
+  }
 }
