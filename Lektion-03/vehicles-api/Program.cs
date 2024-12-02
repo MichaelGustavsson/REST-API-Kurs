70dabd3ad1c6a03c34cb7a12894856b55f806506
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-12-02 17:23:08 +0100

    Solution for lesson 3

--- Lektion-03/vehicles-api/Program.cs
+++ Lektion-03/vehicles-api/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using vehicles_api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Lägga till en tjänst som kopplar oss till vår datalagrings provider
+// Databasen!!!
+builder.Services.AddDbContext<VehicleContext>(options =>
+{
+  options.UseSqlite("Data Source=vehicles.db");
+});
+
 
 builder.Services.AddControllers();
 
