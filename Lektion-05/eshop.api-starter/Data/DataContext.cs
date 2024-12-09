347cac88fc7f1ccd5a4c32bfe8e8b58fc8acbadd
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-12-09 20:45:55 +0100

    Added starter project for todays lesson

---
+++ Lektion-05/eshop.api-starter/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using eshop.api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eshop.api.Data;
+
+public class DataContext : DbContext
+{
+  public DbSet<Product> Products { get; set; }
+  public DataContext(DbContextOptions options) : base(options)
+  {
+  }
+}
---
+++ Lektion-05/eshop.api-starter/Entities/Product.cs
@@ -0,0 +1,9 @@
+namespace eshop.api.Entities;
+
+public class Product
+{
+  public int Id { get; set; }
+  public string ItemNumber { get; set; }
+  public string ProductName { get; set; }
+  public double Price { get; set; }
+}
