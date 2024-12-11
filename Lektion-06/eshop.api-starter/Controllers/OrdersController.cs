ddf201cdf24cd3912dd5c7f757ab07276bd614d4
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-12-11 21:59:31 +0100

    Added starter project for todays walkthrough

---
+++ Lektion-06/eshop.api-starter/Controllers/OrdersController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace eshop.api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+
+}
---
+++ Lektion-06/eshop.api-starter/Entities/OrderItem.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eshop.api.Entities;
+
+public class OrderItem
+{
+  public int SalesOrderId { get; set; }
+  public int ProductId { get; set; }
+  public int Quantity { get; set; }
+  public double Price { get; set; }
+
+  // Navigational properties...
+  public Product Product { get; set; }
+  public SalesOrder SalesOrder { get; set; }
+}
